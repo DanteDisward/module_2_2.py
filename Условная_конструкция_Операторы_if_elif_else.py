46d3d39615c90d852dfa297bfe38398bbeabdaf4
DanteDisward <nikitachernigin@gmail.com>
2024-08-07 00:05:16 +0500

    Create Условная_конструкция_Операторы_if_elif_else.py

---
+++ Условная_конструкция_Операторы_if_elif_else.py
@@ -0,0 +1,19 @@
+import re #Импортируем библиотеку
+
+while True: #Запускем бесконечный цыкл
+    try:
+        first, second, third = re.split(r'[,; ]+', input("Введите три числа: ")) #Просим ввести данные и разделяем строку на несколько значений
+        first = int(first)
+        second = int(second)
+        third = int(third)
+        break #Если всё впорядке, останавливаем цыкл
+    except ValueError:
+        print("Вы допустили ошибку") #Если данные не правильно ввели, выводим соответствующее сообщение
+#Проверяем сколько чисел равны между собой
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else:
+    print(0)
+
